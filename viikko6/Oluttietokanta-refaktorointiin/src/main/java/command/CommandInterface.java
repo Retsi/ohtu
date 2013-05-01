72d6ac123e2da3297a1de6baaea95dfaa6160642
Retsi <retsi@PC-Lauri.(none)>
2013-05-01 21:03:07 +0300

    tehtäviä

---
+++ viikko6/Oluttietokanta-refaktorointiin/src/main/java/command/CommandInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+/**
+ *
+ * @author retsi
+ */
+public interface CommandInterface {
+    
+    public void run();
+    
+}
